f6375ca54d6424608e00017f08a208fb1f2ff3a7
cmdrGuyson <gayangakuruppu@gmail.com>
2021-03-21 01:49:24 +0530

    [+] Access redux actions

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,32 +1,36 @@
-import { SET_AUTHENTICATED, SET_UNAUTHENTICATED } from "../types";
+import {
+  SET_AUTHENTICATED,
+  SET_PLAYLISTS,
+  SET_SONGS,
+  SET_UNAUTHENTICATED,
+} from "../types";
 
 const initialState = {
-	playlist: [],
-	errors: null,
-	authenticated: false,
-	loading: false,
-	user: {},
+  errors: null,
+  authenticated: false,
+  loading: false,
+  user: {},
 };
 
 export default function (state = initialState, action) {
-	switch (action.type) {
-		case SET_AUTHENTICATED: {
-			return {
-				...state,
-				authenticated: true,
-				loading: false,
-			};
-		}
-		case SET_UNAUTHENTICATED: {
-			return {
-				...state,
-				authenticated: false,
-				loading: false,
-				user: {},
-			};
-		}
+  switch (action.type) {
+    case SET_AUTHENTICATED: {
+      return {
+        ...state,
+        authenticated: true,
+        loading: false,
+      };
+    }
+    case SET_UNAUTHENTICATED: {
+      return {
+        ...state,
+        authenticated: false,
+        loading: false,
+        user: {},
+      };
+    }
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
--- src/redux/reducers/uiReducer.js
+++ src/redux/reducers/uiReducer.js
@@ -1,44 +1,72 @@
-import { SET_USER } from "../types";
+import { CLEAR_ERRORS, SET_USER } from "../types";
 
-import { LOADING, SET_ERRORS, SET_CREATED_MESSAGE, SET_ROOMS } from "../types";
+import {
+  LOADING,
+  SET_ERRORS,
+  SET_CREATED_MESSAGE,
+  SET_ROOMS,
+  SET_SONGS,
+  SET_PLAYLISTS,
+} from "../types";
 
 const initialState = {
-	rooms: [],
-	loading: false,
-	created: false,
-	errors: {},
+  rooms: [],
+  loading: false,
+  created: false,
+  errors: {},
+  songs: [],
+  playlists: [],
 };
 
 export default function (state = initialState, action) {
-	switch (action.type) {
-		case LOADING:
-			return {
-				...state,
-				created: false,
-				errors: {},
-				loading: true,
-			};
-		case SET_ERRORS:
-			return {
-				...state,
-				errors: action.payload,
-			};
-		case SET_CREATED_MESSAGE:
-			let allrooms = [...state.rooms];
-			allrooms.push(action.payload);
-			return {
-				...state,
-				rooms: allrooms,
-				loading: false,
-				created: true,
-			};
-		case SET_ROOMS:
-			return {
-				...state,
-				loading: false,
-				rooms: action.payload,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case LOADING:
+      return {
+        ...state,
+        created: false,
+        errors: {},
+        loading: true,
+      };
+    case SET_ERRORS:
+      return {
+        ...state,
+        errors: action.payload,
+      };
+    case SET_CREATED_MESSAGE:
+      let allrooms = [...state.rooms];
+      allrooms.push(action.payload);
+      return {
+        ...state,
+        rooms: allrooms,
+        loading: false,
+        created: true,
+      };
+    case SET_ROOMS:
+      return {
+        ...state,
+        loading: false,
+        rooms: action.payload,
+      };
+
+    case SET_SONGS:
+      return {
+        ...state,
+        songs: action.payload,
+        loading: false,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        errors: {},
+        loading: false,
+      };
+    case SET_PLAYLISTS:
+      return {
+        ...state,
+        playlists: action.playlists,
+        loading: false,
+      };
+    default:
+      return state;
+  }
 }
